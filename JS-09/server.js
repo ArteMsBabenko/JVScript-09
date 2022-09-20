c19c5c07d19095234dbfb69e81abf985afa4812b
ArteMsBabenko <106840102+ArteMsBabenko@users.noreply.github.com>
2022-09-20 21:41:28 +0300

    Update server.js

--- JS-09/server.js
+++ JS-09/server.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var bodyParser = require("body-parser");
+let express = require('express');
+let bodyParser = require("body-parser");
 
-var server = express();
-var jsonParser = bodyParser.json();
+let server = express();
+let jsonParser = bodyParser.json();
 server.use(jsonParser);
 
 server.use(express.static(__dirname));
@@ -13,7 +13,7 @@ server.get("/", function (req, resp) {
 
 server.get("/formGet", function (req, resp) {
     console.log("Form receipted by GET.");
-    var obj = req.query;
+    let obj = req.query;
     console.log(obj.lName += ".ValidatedByGET");
     console.log(obj.fName += ".ValidatedByGET");
     console.log(obj.age += ".ValidatedByGET");
@@ -24,7 +24,7 @@ server.get("/formGet", function (req, resp) {
 server.post("/formPost", function (req, resp) {
     console.log("Form receipted by POST.");
     // console.log(JSON.parse(req.query));
-    var obj = req.body;
+    let obj = req.body;
     console.log(obj.lName += ".ValidatedByPOST");
     console.log(obj.fName += ".ValidatedByPOST");
     console.log(obj.age += ".ValidatedByPOST");
